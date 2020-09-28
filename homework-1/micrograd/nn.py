e92f5d8da1a2a2a8317ab8f9753a35ad340ae45f
Rinat <51479797+Xrenya@users.noreply.github.com>
2020-09-28 15:20:10 +0900

    Update nn.py

--- homework-1/micrograd/nn.py
+++ homework-1/micrograd/nn.py
@@ -27,7 +27,6 @@ class Linear(Module):
 
     def forward(self, inp):
         """Y = W * x + b"""
-        # YOUR CODE GOES HERE
         return # YOUR CODE GOES HERE
 
     def parameters(self):
@@ -45,5 +44,4 @@ class CrossEntropyLoss(Module):
     """Cross-entropy loss for multi-class classification"""
     def forward(self, inp, label):
         # Create CrossEntropy Loss Module
-        # YOUR CODE GOES HERE
         return # YOUR CODE GOES HERE
